7b2f7e84893e9bc57e4e4f10166cd9fc33850ce8
sapunethmini <122308654+sapunethmini@users.noreply.github.com>
2023-03-21 14:06:16 +0530

    Add files via upload

---
+++ web/customer.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html> 
+<html> 
+    <head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: black;
+  padding: 25px 42px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 32px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+/* .iframe {
+  display: inline-block;
+  vertical-align: top;
+} */
+
+.ssp {
+  color:blue;
+}
+
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #333eb2;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.hhh{
+  width: 100%;
+}
+
+.dropdown a:hover {background-color: #ddd;}
+
+.show {display: block;}
+</style>
+
+    </head>
+
+
+<body bgcolor="white">
+
+    <h1 class="ssp">WELCOME !!!</h1><br/><br/>
+
+    <table>
+        <tr>
+        <div class="show">
+            <th><a href="1stpage.php" target="_self" class="button">Home</a></th>
+            <th><a href="veiwproduct.html" target="f1" class="button">Veiw our product</a></th>
+            <th><a href="feedpack.php" target="f1" class="button">Feedback</a></th>            
+           
+       </tr>
+    </table>
+    <hr>
+  <br/>
+  <p></p>
+   
+  <iframe src="cus.png" name="f1" height="900px" width="100%" >
+    </iframe>
+</html>
